e13d445b52f272914cabc5af19be08c32bff2352
Dmitry Kashuba <dmitrykashuba@icloud.com>
2021-02-11 11:23:57 +0200

    Remove storing global state in local storage

--- src/App/GlobalState.tsx
+++ src/App/GlobalState.tsx
@@ -15,57 +15,7 @@ export const useGlobalState: () => GlobalStateContextType = () => useContext(Glo
 
 export default ({children}: PropsWithChildren<{}>) => {
 
-    let hidden: string, visibilityChange: string;
-    if (typeof document.hidden !== "undefined") {
-        hidden = "hidden";
-        visibilityChange = "visibilitychange";
-    } else if (typeof (document as any).msHidden !== "undefined") {
-        hidden = "msHidden";
-        visibilityChange = "msvisibilitychange";
-    } else if (typeof (document as any).webkitHidden !== "undefined") {
-        hidden = "webkitHidden";
-        visibilityChange = "webkitvisibilitychange";
-    } else {
-        visibilityChange = "";
-    }
-
-    const pageHide = "onpagehide" in window ? "pagehide" : "beforeunload";
-
-    const stateKey: string = "state";
-
     const [state, setState] = useState<GlobalStateType>({});
-
-    const storeState = () => {
-        window.localStorage.setItem(stateKey, JSON.stringify(state));
-    };
-
-    const restoreState = () => {
-        const restoredState: GlobalStateType = JSON.parse(window.localStorage.getItem(stateKey) as string);
-        if (restoredState) {
-            setState(restoredState);
-            window.localStorage.removeItem(stateKey);
-        }
-    };
-
-    const beforeUnloadHandler = () => storeState();
-
-    const visibilityChangeHandler = () => (document as any)[hidden]
-        ? storeState()
-        : setTimeout(() => restoreState(), 10);
-
-    useEffect(() => {
-        restoreState();
-    }, []);
-
-    useEffect(() => {
-        window.removeEventListener(pageHide, beforeUnloadHandler, false);
-        document.removeEventListener(visibilityChange, visibilityChangeHandler, false);
-        window.addEventListener(pageHide, beforeUnloadHandler, false);
-        document.addEventListener(visibilityChange, visibilityChangeHandler, false);
-        return () => {
-            document.removeEventListener(visibilityChange, visibilityChangeHandler, false);
-        };
-    });
 
     return (
         <GlobalStateContext.Provider value={{state, setState}}>
